b60df8f909a603531ff698547dbbd2b6c63e91ff
taskpul <romtietz@gmail.com>
2025-10-15 13:55:20 +0200

    Delete resources/themes/drift directory

--- resources/themes/drift/components/app/container.blade.php
+++
@@ -1,3 +0,0 @@
-<div {{ $attributes->merge(['class' => 'relative w-full px-4 mx-auto max-w-7xl md:px-8']) }}>
-    {{ $slot }}
-</div>
--- resources/themes/drift/components/elements/container.blade.php
+++
@@ -1,3 +0,0 @@
-<div {{ $attributes->merge(['class' => 'px-5 mx-auto max-w-7xl md:px-8']) }}>
-    {{ $slot }}
-</div>
